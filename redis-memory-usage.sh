a7d9ac8f02997745743a98623d43a343fa5da6bd
wanghaibo@myhexin.com <wanghaibo@myhexin.com>
2015-06-09 20:46:33 +0800

    add nginx redis script

--- redis-memory-usage.sh
+++ redis-memory-usage.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
-used_memory=`/usr/local/bin/redis-cli info |grep 'used_memory:'|awk -F 'used_memory:' '{printf("%d",$2)}'`
-max_memory=`/usr/local/bin/redis-cli config get maxmemory|grep -v 'memory'`
+redis_cli=`whereis redis-cli|awk '{print $NF}'`
+if [[ $redis_cli =~ ":" ]];then
+    echo "can not find redis-cli"
+    exit
+fi
+used_memory=`$redis_cli info |grep 'used_memory:'|awk -F 'used_memory:' '{printf("%d",$2)}'`
+max_memory=`$redis_cli config get maxmemory|grep -v 'memory'`
+
+#初始化参数
+CRITICAL=100
+#分析参数
+while getopts "c:h" opt
+do
+    case $opt in
+        t ) TYPE=$OPTARG;;
+        w ) WARNING=$OPTARG;;
+        c ) CRITICAL=$OPTARG;;
+        ? ) 
+        echo "-c 剩余容量critical值"
+        exit 1;; 
+    esac
+done
 
 STATE_OK=0   
 STATE_WARNING=1   
@@ -9,10 +29,10 @@ STATE_UNKNOWN=3
 max_memory=$[$max_memory/1024/1024]
 used_memory=$[$used_memory/1024/1024]
 
-if [ $used_memory -lt $(($max_memory-100)) ];then 
+if [ $used_memory -lt $(($max_memory-$CRITICAL)) ];then 
     echo "TEST OK;max_memory is $max_memory M;used_memory is $used_memory M|used=$used_memory" 
     exit $STATE_OK 
-elif [ $used_memory -gt $(($max_memory-100)) ] || [ $used_memory -eq $(($max_memory-100)) ];then 
+elif [ $used_memory -gt $(($max_memory-$CRITICAL)) ] || [ $used_memory -eq $(($max_memory-$CRITICAL)) ];then 
     echo "TEST CRITICAL;max_memory is $max_memory M;used_memory is $used_memory M|used=$used_memory" 
     exit $STATE_CRITICAL
 else  
